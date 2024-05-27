fef213c3a150b9d537842760b82be91be09b2fb4
abategirum <73879476+abategirum@users.noreply.github.com>
2024-05-27 11:43:44 -0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        sout("what a magnifique!");
+        System.out.println("what a magnifique!");
+         System.out.println("oh lala!");
     }
 }
